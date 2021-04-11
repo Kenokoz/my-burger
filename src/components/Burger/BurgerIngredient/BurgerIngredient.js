8bf14793a67dfcb419547e89137d4db122bf0c2a
Kirill Andreev <kenokoz@yandex.ru>
2021-04-11 16:53:33 +0300

    Add outputting burger ingredients dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -24,10 +24,10 @@ class BurgerIngredient extends Component {
       case 'cheese':
         ingredient = <div className="Cheese"></div>;
         break;
-      case 'Salad':
+      case 'salad':
         ingredient = <div className="Salad"></div>;
         break;
-      case 'Bacon':
+      case 'bacon':
         ingredient = <div className="Bacon"></div>;
         break;
       default:
