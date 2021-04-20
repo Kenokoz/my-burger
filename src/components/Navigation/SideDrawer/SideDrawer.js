129a76808ff56bb1cc64c6eb42468509c65efd38
Kirill Andreev <kenokoz@yandex.ru>
2021-04-20 18:28:04 +0300

    Add the disappearance of side drawer after click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/AuxWrapper';
 import './SideDrawer.css';
 
 const sideDrawer = props => {
+  let attachedClasses = ['SideDrawer', 'Close'];
+
+  if (props.open) {
+    attachedClasses = ['SideDrawer', 'Open'];
+  }
+
   return (
-    <div className="SideDrawer">
-      <Logo height="11%" />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <Logo height="11%" />
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
